92e6c7e86fa3f9692310ea0634ccd9c713041ff7
010Harsh010 <harshsingh794613@gmail.com>
2025-03-16 20:04:30 +0530

    change Port

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,7 @@ import { initializeSocket } from "./socket.js";
 import { createClient } from "redis";
 
 const app = express();
-const whitelist = ["http://localhost:5173"];
+const whitelist = [process.env.URL];
 
 const corsOptions = {
   origin: function (origin, callback) {
