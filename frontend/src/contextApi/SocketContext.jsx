e19198299561d485c250ee857205b1e17405801e
010Harsh010 <harshsingh794613@gmail.com>
2025-03-16 22:49:25 +0530

    again socket

--- frontend/src/contextApi/SocketContext.jsx
+++ frontend/src/contextApi/SocketContext.jsx
@@ -5,7 +5,7 @@ import { io } from 'socket.io-client';
 export const SocketContext = createContext(null);
 
 const SocketProvider = ({children}) => {
-    const socket = io("https://streamx-8kxh.onrender.com");
+    const socket = io("https://streamx-8kxh.onrender.com", { transports: ["websocket"] });
     useEffect(() => {
         socket.on('connect', () => {
             console.log('Connected to server');
